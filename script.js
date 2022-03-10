8dfebb4b6f17107773ffc05fac408e14ad2aaa35
Well <wellington.multilaser@gmail.com>
2022-03-10 17:40:29 -0300

    movimetos da cobrinha

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let canvas = document.getElementById("cobrinha");
-let context = canvas.getContext ("2d");
+let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] = {
-    x: 8 * box.
+    x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG() {
     context.fillStyle = "Lightgree";
@@ -18,5 +19,27 @@ function criarCobrinha() {
         context.fillRect (snake[1].x, snake[i].y, box, box);
 }
 
-criarBG();
-criarCobrinha
+
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha(); 
+    
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -=box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+    
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
